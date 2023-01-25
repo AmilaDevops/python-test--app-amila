380796c662acd03de7ff21a9150e79b1c4232ce4
Amila Devops <amila.kdam@gmail.com>
2023-01-25 15:06:41 +0530

    adding commands

--- docker_push_hub.sh
+++ docker_push_hub.sh
@@ -3,5 +3,11 @@
 #login to docker hub with your docker hub user name and password for push my local docker image to dockerhub
 docker login --username amila11111 --password-stdin
 
+#build the python-web server docker image with docker-compose
+docker-compose build
+
+#run docker container with docker-compose
+docker-compose up -d
+
 #pushing my docker image to my docker hub account
 docker push amila11111/python_http_server:1
